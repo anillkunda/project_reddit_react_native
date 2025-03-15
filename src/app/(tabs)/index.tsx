04c8f1ec4357d544490f110e88eee0f3e83d4318
Anil Kunda <anilkunda701@gmail.com>
2025-03-15 19:35:01 +0530

    finish bottom tabs

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,10 +1,9 @@
 import { Text, View } from "react-native";
-import { Link } from "expo-router";
 
 export default function HomeScreen() {
   return (
     <View>
-      <Link href="about">Hello, World Expo Router!</Link>;
+      <Text>Home Screen</Text>
     </View>
   );
 }
--- src/app/about.tsx
+++
@@ -1,5 +0,0 @@
-import { Text } from "react-native";
-
-export default function AboutScreen() {
-  return <Text>Hello form About!</Text>;
-}
